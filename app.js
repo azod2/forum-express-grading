571c3837f1edc67e8b0472518996e9c00ad886e2
Jo <a0750318@gmail.com>
2021-11-13 16:06:16 +0800

    add restController, restaurants router, main and restaurant.hbs, add hbs engine on app.js

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const handlebars = require('express-handlebars');
+
 const app = express()
 const port = 3000
+
+
+// 設定 view engine 使用 handlebars
+app.engine('hbs', handlebars({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
+// 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
 require('./routes')(app)
 
 module.exports = app
